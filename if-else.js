f1e7458ef68acf9df0a0331922f1b2296f3730d8
mahmudaakter63 <mahmudaakter2632@gmail.com>
2024-10-25 20:15:03 +0600

    first commit

---
+++ if-else.js
@@ -0,0 +1,29 @@
+const number = 20;
+const x = 40;
+var y = 30;
+let p = 100;
+if (number < 18) {
+  console.log("Morning");
+}
+else if( number<25 ){
+    console.log("mhmuda");
+}
+if (x < 88) {
+    console.log("Midday");
+  }
+  else if( x<9){
+      console.log("Afternoon");
+  }
+  
+  if (y < 55) {
+    console.log("Evenning");
+  }
+  else if( y<20){
+      console.log("Night");
+  }
+  if (p < 5) {
+    console.log("Midnight");
+  }
+  else if( p<200){
+      console.log("It's 24 hours.");
+  }
